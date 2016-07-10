a6dceedaa79d19272726c5cf80617d4c68116ba3
Aasish Khanal <Aasish Khanal>
2016-07-10 18:16:26 +1200

    adding script content php file

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+print "
+<link rel='stylesheet' type='text/css' href='bootstrap/css/bootstrap.min.css'/>
+
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js'>
+</script>
+<script src='bootstrap/js/bootstrap.min.js'></script>";
+
+?>
