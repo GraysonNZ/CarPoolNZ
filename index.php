d7daaf1bbd7db49c8a9e31c76c1e8693b6200f89
Aasish Khanal <Aasish Khanal>
2016-07-10 17:45:44 +1200

    Adding Jumbotron

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Carpool- New Zealand</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js'>
+</script>
+<script src='bootstrap/js/bootstrap.min.js'></script>
 </head>
 
 <body>
- The Setup Has been successful.
+
+   <div class="container">
+  <div class="jumbotron">
+    <h1>Share The Ride</h1> 
+    <p>Reduce your Travel Expense. Either be the passenger and travel with others or be the driver and share your ride.
+     It's that simple.</p> 
+  </div>
+  
+  <p> The content File goes here </p>
+  
+  
+</div>
+ 
 </body>
 </html>
