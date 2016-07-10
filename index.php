9ec918b951d59decefec259407950d542a86803a
Aasish Khanal <Aasish Khanal>
2016-07-10 18:15:11 +1200

    Updated Index.php- Added Header

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
+<?php
+	include('header.php');
+	
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Carpool- New Zealand</title>
-<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"/>
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js'>
-</script>
-<script src='bootstrap/js/bootstrap.min.js'></script>
+<?php
+	include('script.php');
+?>
 </head>
 
 <body>
@@ -18,7 +22,7 @@
      It's that simple.</p> 
   </div>
   
-  <p> The content File goes here </p>
+  <p> Please <a href="login.php"> Login here </a> to share the ride. </p>
   
   
 </div>
